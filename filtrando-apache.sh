857f11a80394c8a7962be6bbcfc4e4d858b501c5
Wallace <wrsantos.developer@gmail.com>
2018-08-21 15:50:17 -0300

    Adicionando verificação de IP

--- filtrando-apache.sh
+++ filtrando-apache.sh
@@ -4,9 +4,13 @@ regex="\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 
 if [[ $1 =~ $regex ]]
 then
-	echo "entrou"
-	cat apache-log/apache.log | grep $1
+	cat ~/Projetos/Scripts/logs/apache-log/apache.log | grep $1
+	
+	if [ $? -ne 0 ]
+	then
+		echo "Ip não encontrado no arquivo de log. "
+	fi
+
 else
 	echo "IP inválido"	
 fi
-
